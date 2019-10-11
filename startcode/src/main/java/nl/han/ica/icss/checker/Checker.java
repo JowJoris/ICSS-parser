8c41d0267ec83f8ed02484e70a99dc61e5319a4e
Joris Huinink <joris.huinink@gmail.com>
2019-10-11 23:31:49 +0200

    Refactored variable defined check

--- startcode/src/main/java/nl/han/ica/icss/checker/Checker.java
+++ startcode/src/main/java/nl/han/ica/icss/checker/Checker.java
@@ -10,6 +10,7 @@ import nl.han.ica.icss.ast.types.*;
 public class Checker {
 
     private LinkedList<HashMap<String,ExpressionType>> variableTypes;
+    private HashMap<String, ExpressionType> variableHashMap = new HashMap<>();
 
     public void check(AST ast) {
         variableTypes = new LinkedList<>();
@@ -18,34 +19,42 @@ public class Checker {
 
     //Check if variables are defined
     private void checkVariables(ASTNode node){
+        findVariableAssignment(node);
+        variableTypes.add(variableHashMap);
+        findVariableReference(node);
+    }
+
+    private void findVariableAssignment(ASTNode node) {
         //Find every node which is an instance of VariableAssignment
         if(node instanceof VariableAssignment){
             String name = ((VariableAssignment) node).name.name;
             Expression e = ((VariableAssignment) node).expression;
-            HashMap<String, ExpressionType> hashMap = new HashMap<>();
-            variableTypes.add(hashMap);
-            hashMap.put(name, expressionType(e));
+            variableHashMap.put(name, expressionType(e));
         }
         node.getChildren().forEach(this::checkVariables);
+    }
 
-        //Find every node which is an instance of VariableReference
+    //Find every node which is an instance of VariableReference
+    private void findVariableReference(ASTNode node) {
         if(node instanceof VariableReference){
-           String name = ((VariableReference) node).name;
-           boolean varExists = false;
+            String name = ((VariableReference) node).name;
+            boolean varExists = checkVariableReferenceToVariableAssignment(name);
 
-           //Check if the VariableReference node also belongs to VariableAssignment
-           for(HashMap<String, ExpressionType> varHash: variableTypes){
-               if(varHash.containsKey(name)){
-                   varExists = true;
-               }
-           }
-
-           //Set error if VariableReference node doesn't belong to VariableAssignment
-           if(!varExists){
-               node.setError("Variable reference: " + ((VariableReference) node).name + " is undefined");
-           }
-
+            //Set error if VariableReference node doesn't belong to VariableAssignment
+            if(!varExists){
+                node.setError("Variable reference: " + ((VariableReference) node).name + " is undefined");
+            }
         }
+    }
+
+    //Check if the VariableReference node also belongs to VariableAssignment
+    private boolean checkVariableReferenceToVariableAssignment(String name) {
+        for(HashMap<String, ExpressionType> varHash: variableTypes){
+            if(varHash.containsKey(name)){
+               return true;
+            }
+        }
+        return false;
     }
 
     //Get the ExpressionType from Expression
