bb1cab976f85b7b80a756c0475c2274432ff44ee
Joris Huinink <joris.huinink@gmail.com>
2019-10-17 18:19:52 +0200

    Added calculations of expressions

--- startcode/src/main/java/nl/han/ica/icss/transforms/EvalExpressions.java
+++ startcode/src/main/java/nl/han/ica/icss/transforms/EvalExpressions.java
@@ -1,15 +1,25 @@
 package nl.han.ica.icss.transforms;
 
 import nl.han.ica.icss.ast.*;
+import nl.han.ica.icss.ast.literals.PixelLiteral;
+import nl.han.ica.icss.ast.literals.ScalarLiteral;
+import nl.han.ica.icss.ast.operations.AddOperation;
+import nl.han.ica.icss.ast.operations.MultiplyOperation;
+import nl.han.ica.icss.ast.operations.SubtractOperation;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Stack;
+
+import static nl.han.ica.icss.transforms.OperationSide.*;
 
 public class EvalExpressions implements Transform {
 
     private LinkedList<HashMap<String, Literal>> variableValues;
-    private HashMap<String, Expression> variableValuesHashMap;
+    private HashMap<String, Literal> variableValuesHashMap;
+    private LinkedList<Expression> list;
+    private Stack<Expression> stack;
 
     public EvalExpressions() {
         variableValues = new LinkedList<>();
@@ -19,40 +29,59 @@ public class EvalExpressions implements Transform {
     public void apply(AST ast) {
         variableValues = new LinkedList<>();
         variableValuesHashMap = new HashMap<>();
+        list = new LinkedList<>();
+        stack = new Stack<>();
         retrieveAllVariables(ast.root);
-        findDeclarationVariables(ast.root);
+        findExpressionsAndOperationsInDeclarations(ast.root, ast.root.getChildren());
+        findOperations(ast.root, ast.root.getChildren());
+        list.forEach(i -> System.out.println(i + " "));
     }
 
-    private void findDeclarationVariables(ASTNode node){
-        if (node instanceof Declaration){
-            findVariableInDeclaration(node);
+    private void findExpressionsAndOperationsInDeclarations(ASTNode parent, ArrayList<ASTNode> children) {
+        for (ASTNode child : children) {
+            if (parent instanceof Declaration && child instanceof Expression) {
+                findVariableReferenceInExpression((Declaration) parent, (Expression) child);
+            }
+            if (parent instanceof Operation) {
+                findVariableReferenceInOperation((Operation) parent);
+            }
+
+            findExpressionsAndOperationsInDeclarations(child, child.getChildren());
         }
-        node.getChildren().forEach(this::findDeclarationVariables);
     }
 
-    private void findVariableInDeclaration(ASTNode node) {
-        VariableReference variableNode = retrieveDeclarationVariable(node.getChildren());
-        if(variableNode != null) {
-            node.removeChild(variableNode);
-            node.addChild(setDeclarationValueFromVariable(variableNode));
+    private void findVariableReferenceInOperation(Expression parent) {
+        if (parent instanceof Operation) {
+            findVariableReferenceInSides(parent, ((Operation) parent).lhs, LEFT);
+            if (((Operation) parent).rhs instanceof Operation) {
+                for (ASTNode node : ((Operation) parent).rhs.getChildren()) {
+                    findVariableReferenceInOperation((Expression) node);
+                }
+            } else if (((Operation) parent).rhs instanceof VariableReference) {
+                findVariableReferenceInSides(parent, ((Operation) parent).rhs, RIGHT);
+            }
         }
     }
 
-    private ASTNode setDeclarationValueFromVariable(VariableReference variableNode) {
-        if(variableValuesHashMap.containsKey(variableNode.name)){
+    private void findVariableReferenceInSides(ASTNode parent, Expression child, OperationSide side) {
+        if (child instanceof VariableReference) {
+            if (side == LEFT) {
+                ((Operation) parent).lhs = getDeclarationValueFromVariable((VariableReference) child);
+            } else if (side == RIGHT) {
+                ((Operation) parent).rhs = getDeclarationValueFromVariable((VariableReference) child);
+            }
+        }
+    }
+
+    private void findVariableReferenceInExpression(Declaration parent, Expression child) {
+        if (child instanceof VariableReference) {
+            parent.expression = getDeclarationValueFromVariable((VariableReference) child);
+        }
+    }
+
+    private Literal getDeclarationValueFromVariable(VariableReference variableNode) {
+        if (variableValuesHashMap.containsKey(variableNode.name)) {
             return variableValuesHashMap.get(variableNode.name);
-        }
-        return null;
-    }
-
-    private VariableReference retrieveDeclarationVariable(ArrayList<ASTNode> children) {
-        for(ASTNode node: children){
-            if(node instanceof VariableReference){
-                return (VariableReference) node;
-            }
-            if(node instanceof Operation){
-                return retrieveDeclarationVariable(node.getChildren());
-            }
         }
         return null;
     }
@@ -60,9 +89,77 @@ public class EvalExpressions implements Transform {
     private void retrieveAllVariables(ASTNode node) {
         if (node instanceof VariableAssignment) {
             String name = ((VariableAssignment) node).name.name;
-            Expression e = ((VariableAssignment) node).expression;
-            variableValuesHashMap.put(name, e);
+            Literal l = (Literal) ((VariableAssignment) node).expression;
+            variableValuesHashMap.put(name, l);
         }
         node.getChildren().forEach(this::retrieveAllVariables);
+    }
+
+    private void findOperations(ASTNode parent, ArrayList<ASTNode> children) {
+        for (ASTNode child : children) {
+            if (parent instanceof Declaration && child instanceof Operation) {
+                list.clear();
+                fillStackWithOperations(null, (Operation) child);
+                parent.removeChild(child);
+                parent.addChild(calculatePostFixStack());
+            }
+            findOperations(child, child.getChildren());
+        }
+    }
+
+    private PixelLiteral calculatePostFixStack() {
+        for (Expression e : list) {
+            if (e instanceof Operation) {
+                int left = findExpressionValue(stack.peek());
+                stack.pop();
+                int right = findExpressionValue(stack.peek());
+                stack.pop();
+                stack.push(calculateOperation((Operation) e, left, right));
+            } else {
+                stack.push(e);
+            }
+        }
+        return new PixelLiteral(findExpressionValue(stack.peek()));
+    }
+
+    private Expression calculateOperation(Operation e, int left, int right) {
+        if (e instanceof MultiplyOperation) {
+            return new ScalarLiteral(left * right);
+        } else if (e instanceof AddOperation) {
+            return new ScalarLiteral(left + right);
+        } else {
+            return new ScalarLiteral(left - right);
+        }
+    }
+
+    private void fillStackWithOperations(MultiplyOperation mul, Operation node) {
+        list.add(node.lhs);
+        if (mul != null) {
+            list.add(mul);
+        }
+        if (node.rhs instanceof Operation) {
+            if (node instanceof MultiplyOperation) {
+                fillStackWithOperations(new MultiplyOperation(), (Operation) node.rhs);
+            } else {
+                fillStackWithOperations(null, (Operation) node.rhs);
+            }
+        } else {
+            list.add(node.rhs);
+        }
+        if (node instanceof AddOperation) {
+            list.add(new AddOperation());
+        } else if (node instanceof MultiplyOperation && !(node.rhs instanceof Operation)) {
+            list.add(new MultiplyOperation());
+        } else if (node instanceof SubtractOperation) {
+            list.add(new SubtractOperation());
+        }
+    }
+
+    private int findExpressionValue(Expression node) {
+        if (node instanceof PixelLiteral) {
+            return ((PixelLiteral) node).value;
+        } else {
+            return ((ScalarLiteral) node).value;
+        }
     }
 }
